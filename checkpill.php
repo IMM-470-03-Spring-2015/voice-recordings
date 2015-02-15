4b47c67650978919cd9ec8d379d14e670bb8cf71
Mark D. Thompson <thomesoni@gmail.com>
2015-02-15 10:07:55 -0500

    updated

--- checkpill.php
+++ checkpill.php
@@ -6,15 +6,15 @@ echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
 
 if($pill == 1){ // red pill
     echo "<Response>";
-    echo "<Say></Say>";
+    echo "<Play>http://www.mediamesis.net/imm/bluepill.mp3</Play>";
     echo "</Response>";
-} else if($pill = 2) { // blue pill
+} else if($pill == 2) { // blue pill
     echo "<Response>";
-    echo "<Say></Say>
+    echo "<Play>http://www.mediamesis.net/imm/matrix66.mp3</Play>";
     echo "</Response>";
 } else { // other
     echo "<Response>";
     echo "<Say>Sorry, I didn't understand your selection. Please try again.</Say>"; 
-    echo "<Redirect method="POST">http://yourserver/matrix.xml</Redirect>";
+    echo "<Redirect method=\"POST\">http://www.tcnj.edu/~thompsom/imm-470-03/voice-recordings/matrix.xml</Redirect>";
     echo "</Response>";
 }
