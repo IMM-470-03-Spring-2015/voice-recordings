94221b01a63eb5a8977e24b4372a5f7f6b2c3440
Mark D. Thompson <thomesoni@gmail.com>
2016-02-25 20:35:17 -0500

    Create save2sc.php

---
+++ save2sc.php
@@ -0,0 +1,52 @@
+<?php
+if( isset($_REQUEST["recid"]) && !empty($_REQUEST["recid"]) ) {
+    
+    // include the SoundCloud library
+    require('Services/Soundcloud.php');
+
+    // Twilio stuff
+    $accountSID = 'ACc97cde1b267c161a9e1d2f916b75d22b';
+    $recordingSID = htmlspecialchars($_REQUEST["recid"]);
+    // space seperated list of tags
+    $tags = ( isset($_REQUEST["tag"]) && !empty($_REQUEST["tag"]) ) ? $_REQUEST["tag"] : "general" ;
+
+    // download the twilio recording
+    $remotefile = "https://api.twilio.com/2010-04-01/Accounts/".$accountSID."/Recordings/".$recordingSID.".mp3?Download=true";
+
+    if(!@copy($remotefile,'./downloaded.mp3')) {
+        $errors= error_get_last();
+        echo "COPY ERROR: ".$errors['type']."<br />";
+        echo "<br />\n".$errors['message'];
+    } else {
+        echo "File copied from remote!<br />";
+    }
+
+    // set the path to the sound file
+    $file = '@'.realpath(dirname(__FILE__)).'/'.'downloaded.mp3';
+
+    // SoundCloud stuff
+
+    // create a new SoundCloud Object...
+    $soundcloud = new Services_Soundcloud('8c2973e42617de13c93c6d601b018b16', '6a56c849d46a4a650523de87701b2108', null);
+    // and get an authentication token bypassing the Oauth connect flow
+    $soundcloud->credentialsFlow('thomesoni@gmail.com','0D1NRULEs');
+
+    // create the track meta data
+    $track = array(
+        'track[title]' => $recordingSID,
+        'track[tag_list]' => $tags,
+        'track[asset_data]' => $file,
+        'track[sharing]' => 'private' //'public'
+    );
+
+    echo '<pre>'.print_r($track,1).'</pre>';
+
+    // try to upload it to SoundCloud
+    try {
+        $response = $soundcloud->post('/tracks', $track);
+    } catch (Services_Soundcloud_Invalid_Http_Response_Code_Exception $e) {
+        exit($e->getMessage());
+    }
+} else {
+    echo "You need to provide a recording sid: e.g. wwww.yourserver.com/save2sc.php?recid=xxxxxxxxxxxx&tag=oral-histories";
+}
